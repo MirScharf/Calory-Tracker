71a3e16af33cf4b0cbc015007b00a77037ddb20f
Mirko Scharf <mirko_s@hotmail.de>
2023-10-17 11:36:56 +0200

    Initial commit

--- backend/models/authController.js
+++ backend/models/authController.js
@@ -1,10 +1,11 @@
+import { request, response } from "express";
 import { createSecretToken } from "./secretToken.js";
 import { User } from "./userModels.js";
-// import bcrypt from "bcryptjs"
+import bcrypt from "bcrypt"
 
 
 // Checks if User (in request.body) is new. Creates new User 
-const login = async (request, response, next) => {
+const register = async (request, response, next) => {
     try {
         const { username, password } = request.body;
         const userExists = await User.findOne({ username })
@@ -17,11 +18,40 @@ const login = async (request, response, next) => {
             withCredentials: true,  // allows cookie to be sent with corss-origin requests (CORS)
             httpOnly: false,  // allow client-side js-code tho access the cookie 
         })
-        response.status(201).json({ message: "User sign in succesfully", success: true, user}) // 201: Created (new session established)
+        response.status(201).json({ message: "User registered succesfully", success: true, user}) // 201: Created (new session established)
+        next()
     }
     catch (error) {
         console.log(error.message);
         response.send({ message: error.message});
     }
 }
-export { login }
+
+const login = async (request,response, next) => {
+    try{
+        const { username, password} = request.body
+        if (!username || !password){
+            return response.json({ message: 'All fields are required.'})
+        }
+        const user = await User.findOne({ username })
+        if (!user) {
+            return response.json({ message: 'Incorrect username or password'})
+        }
+        const auth = await bcrypt.compare(password, user.password)
+        if (!auth) {
+            return response.json({ message: ' Incorrect username or password'})
+        }
+        const token = createSecretToken(user._id)
+        response.cookie("token", token, {
+            withCredentials: true,  // allows cookie to be sent with corss-origin requests (CORS)
+            httpOnly: false,  // allow client-side js-code tho access the cookie 
+        })
+        response.status(201).json({ message: "User logged in succesfully", success: true, user}) // 201: Created (new session established)
+        next()
+    } catch (error) {
+        console.log(error.message);
+        response.json({ message: error.message});
+    }
+}
+
+export { register, login }
