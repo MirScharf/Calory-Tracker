a2b3cf8319b5217908f9f26f8c79b1b5de0aabc9
Mirko Scharf <mirko_s@hotmail.de>
2023-11-10 19:19:54 +0100

    post and get route for Caloric intake

--- backend/models/userRoute.js
+++ backend/models/userRoute.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { Meal } from "./recipyModels.js";
+import { User } from "./userModels.js"
 
 const router = express.Router();
 
@@ -29,5 +30,41 @@ const postRecipy = async(request, response)=> {
     }
 };
 
-router.post('/postRecipy', postRecipy)
+const postCalories = async(request, response)=> {
+    try {
+        if (!request.body.calories){
+            return response.status(400).send({
+            message :"Send all required fields: title, author, ingredients, calories"
+        });
+        }
+        const { user, day, calories } = request.body;
+        const caloryThisDay = await User.findOneAndUpdate(
+            { username: user },  // filter criteria 
+            { $push: { caloryIntake: { day, calories } } },
+            { new: true }
+            // {username: user}, {$push: {"caloryIntake$day": day}}
+            ) 
+        return response.status(201).send( caloryThisDay )
+    } catch (error) {
+        response.status(500).send({ message: error.message});
+    }
+
+}
+
+const getCalories = async(request, response) => {
+    try {
+        const { username } = request.body;
+        console.log(username);
+        const userdata = await User.findOne({username});
+        if (!username){return response.send( {message: "No username in request" })}
+        console.log(userdata.caloryIntake);
+        return response.json(userdata.caloryIntake)
+    } catch (error) {
+        response.status(500).json({ message: 'Server Error'})
+    }
+}
+
+router.post('/postrecipy', postRecipy)
+router.post('/postcalories', postCalories)
+router.post('/getcalories', getCalories)
 export default router; 
