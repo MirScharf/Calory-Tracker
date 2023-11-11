a091361cb4bc3bfd23af04fbfdf221767d577f72
Mirko Scharf <mirko_s@hotmail.de>
2023-11-11 21:23:28 +0100

    bugfix

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,4 @@
 export {default as Login} from './Login';
 export {default as Register} from './Register';
 export {default as Home} from './home';
-export {default as RecipyHub} from './recipyHub';
-
+export {default as RecipyHub} from './RecipyHub';
